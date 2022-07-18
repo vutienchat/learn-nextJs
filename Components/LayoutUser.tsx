c4e91e1251224998d450e903707eadcfb7216902
vutienchat <vutienchat2704@gmail.com>
2022-07-18 10:57:47 +0700

    add component footer

--- Components/LayoutUser.tsx
+++ Components/LayoutUser.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { LayoutProps } from "../utils/interface";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const LayoutUser = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   );
 };
